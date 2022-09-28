70f2d06467d3fd8c74748b735fcd0b8ca25febd2
Minjun <84955607+dev-yuka05@users.noreply.github.com>
2022-09-28 12:47:46 +0900

    Add files via upload

---
+++ order/app.js
@@ -0,0 +1,15 @@
+document.querySelector('.order-modal').addEventListener('click',()=>{
+    document.querySelector('.order').style.display = 'block';
+});
+
+document.querySelector('.order .close').addEventListener('click',()=>{
+    document.querySelector('.order').style.display = 'none';
+});
+
+document.querySelector('.order .apply').addEventListener('click',()=>{
+    document.querySelector('.order').style.display = 'none';
+});
+
+document.querySelector('.order-modal').addEventListener('click',()=>{
+    document.querySelector('.order').style.display = 'block';
+});
